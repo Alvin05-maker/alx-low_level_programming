e6c8741b60b1694a61dd5bfe1a6c8baae99f7386
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-02-17 12:43:50 -0800

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,12 @@ int main(void)
 	char c;
 
 	c = 'a';
-	while (c <= 'z' && c != 'q' && c != 'e')
-	{
+	while (c <= 'z')
+		if ((c != 'q' && c != 'e') && c <= 'z')
+		{
 		putchar(c);
 		c++;
-	}
+		}
 	putchar('\n');
 	return (0);
 }
