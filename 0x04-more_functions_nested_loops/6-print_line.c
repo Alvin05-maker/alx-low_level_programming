c9ed86f81192b2fcac2cb540ee6f6c9be2d071ae
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-02-23 15:46:57 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * print_line - prints line
+ * @n - number of times the charcter '_' should be printed
+ * return: returns line
+ */
+void print_line(int n)
+{
+	int nCopy = n;
+
+	while (nCopy > o)
+	{
+		putchar('_');
+		nCopy--;
+	}
+	else
+	{
+		return (\n);
+	}
+}
+
