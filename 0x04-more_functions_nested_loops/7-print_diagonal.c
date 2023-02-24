37b4214b393ece18af1d074de14fdc4bcee30c42
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-02-23 16:05:20 -0800

    print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * print_diagonal - prints diagonal line
+ * @n - number of times the charcter '\' should be printed
+ * return: ......
+ */
+void print_line(int n)
+{
+	int nCopy = n;
+
+	while (nCopy > o)
+	{
+		putchar('\');
+		nCopy--;
+	}
+	else
+	{
+		return (\n);
+	}
+}
+
