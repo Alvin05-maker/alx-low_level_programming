7f4dce445fda078f2861714cf7f9e467cbfa7c49
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-03-12 05:48:17 -0700

    print alphabet in lowercase 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,22 +3,20 @@
 
 /**
  * print_alphabet_x10 -prints alphabet in lowercase 10 times
- * @c: lowercase alphabets to be printed
- * @i: the number of times that c should be printed
  * Return: return c
  */
 
 void print_alphabet_x10(void)
 {
-    char c;
-    int i;
-
-    for (i = 1; i <= 10; i++)
-    {
-            for (c = 'a'; c <= 'z'; c++)
-            {
-                    putchar(c);
-            }
-            putchar('\n');
-    }
+	char c;
+	int i;
+	
+	for (i = 1; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	}
 }
